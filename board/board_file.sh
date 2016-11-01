93599dec1c7ed2bdb1b9da350029161695728684
utsubo <utsubo@qri.jp>
2016-11-01 15:15:42 +0900

    add

---
+++ board/board_file.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+get_code(){
+ruby << Eof
+print "$1".split("/")[4][3..6]
+Eof
+}
+
+urls=`cat board_file.txt`
+
+for url in $urls;do
+  code=`get_code $url`
+  for i in `seq 1 100`;do
+    echo "ruby board.rb /home/admin/data/board $code $url/$i"
+    ruby board.rb /home/admin/data/board $code $url/$i
+    if [ $? = 1 ];then
+      break
+    fi
+    sleep 1
+  done
+done
+
