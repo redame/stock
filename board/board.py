fe459dc88d5a032eccae01217b89ae8781917173
靱 勝彦 <utsubo@qri.jp>
2016-11-02 13:57:30 +0900

    add

--- board/board.py
+++ board/board.py
@@ -8,11 +8,12 @@ import re
 import os
 import sys
 import datetime
-
+import lxml.html
 
 class YahooBoard:
     def __init__(self, outdir):
         self.outdir = outdir
+        self.date_limit=datetime.datetime.strptime("2015-01-01 00:00:00", '%Y-%m-%d %H:%M:%S')
 
     def execute(self, code, topurl):
 
@@ -31,73 +32,121 @@ class YahooBoard:
     def get_data(self, code, url):
         try:
             iswrite = False
-            doc = urllib.urlopen(url)
-            lst = doc.xpath('//div[@id="cmtlst"]').xpath('.//ul[@class="commentList"]/li')
+            html=urllib.urlopen(url).read()
+            html=re.sub("\r","",html)
+            html=re.sub("\n","",html)
+            html=re.sub("<br.*?>","",html)
+            doc = lxml.html.fromstring(html)
+
+            lst = doc.xpath('//div[@id="cmtlst"]/ul[@class="commentList"]/li')
+
             for v in lst:
                 hash = {}
-                ele = v.xpath('.//div[@class="comment"]')
-                # num=trim(ele.xpath('.//span[@class="comNum"]').text).to_i
-                num = ele.xpath('.//span[@class="comNum"]').text.strip
-                num = re.sub("（.*\）", "", num)
+                ele_tmp = v.xpath('.//div[@class="comment"]')
+                if len(ele_tmp)==0:
+                    continue
+
+                ele=ele_tmp[0]
+
+                num=0
+                num_tmp = ele.xpath('.//span[@class="comNum"]/span')
+                if len(num_tmp)>0:
+                    num=num_tmp[0].text
+                    num = int(re.sub(u"（.*\）", "", num))
+                else:
+                    num_tmp = ele.xpath('.//span[@class="comNum"]')
+                    if len(num_tmp) > 0:
+                        num = num_tmp[0].text
+                        num = int(re.sub(u"（.*\）", "", num))
                 if num == 0:
                     continue
 
                 filename = self.getfilename(url, num)
                 if os.path.exists(filename):
+                    iswrite = True
                     continue
 
                 hash["num"] = num
                 hash["stockCode"] = code
-
-                uele = ele.xpath('./div/p[@class="comWriter"]/a')
+                uele = ele.xpath('./div/p[@class="comWriter"]/a/@href')
                 for v in uele:
-                    hash["user"] = re.sub("^.*?user=", "", str(v["href"]))
+                    hash["user"] = re.sub("^.*?user=", "", v)
 
-                ymd = self.conv_ymd(ele.xpath('./div/p/span/a').text)
-                hash["date"] = ymd
+                s_tmp=ele.xpath('./div/p/span/a')
+                if len(s_tmp)>0:
+                    s=s_tmp[0]
+                    s=s.text.encode("utf-8")
+                    ymd = self.conv_ymd(s)
+                    # date limit check
+                    if ymd<self.date_limit:
+                        return False
 
-                eele = ele.xpath('./div/p[@class="comWriter"]/span[starts-with(@class,"emotionLabel")]')
-                if eele.size > 0:
-                    tmp = eele.attribute("class").text
+                    hash["date"] = ymd.strftime("%Y-%m-%d %H:%M:%S")
+                else:
+                    continue
+
+                eele = ele.xpath('./div/p[@class="comWriter"]/span[starts-with(@class,"emotionLabel")]/@class')
+                if len(eele) > 0:
+                    tmp = eele[0]
                     tmp = re.sub("emotionLabel", "", tmp)
                     tmp = re.sub(" ", "", tmp)
                     hash["emotion"] = tmp
                 else:
                     hash["emotion"] = ""
 
-                replyto = ele.xpath('./p[@class="comReplyTo"]/a').text.trim
-                replyto = int(re.sub(">", "", replyto))
-                if replyto != 0:
-                    hash["replyto"] = replyto
+                hash["replyto"] = ""
+                replyto_tmp = ele.xpath('./p[@class="comReplyTo"]/a')
+                if len(replyto_tmp)>0:
+                    replyto=replyto_tmp[0]
+                    replyto=replyto.text.encode("utf-8")
+                    replyto = int(re.sub(">", "", replyto))
+                    if replyto != 0:
+                        hash["replyto"] = replyto
 
-                body = ele.xpath('./p[@class="comText"]').text.trim
-                hash["body"] = body
 
-                comLike = ele.xpath('./div/ul[@class="comLike cf"]')
-                posi = int(comLike.xpath('./li[@class="positive"]/a/span').text)
-                nega = int(comLike.xpath('./li[@class="negative"]/a/span').text)
-                hash["positive"] = posi
-                hash["negative"] = nega
+
+                body_tmp = ele.xpath('./p[@class="comText"]')
+                if len(body_tmp)==0:
+                    continue
+                body=body_tmp[0]
+                print body.text
+                body = body.text.encode("utf-8")
+                hash["body"] = str(body)
+
+                comLike_tmp = ele.xpath('./div/ul[@class="comLike cf"]')
+                hash["positive"] = ""
+                hash["negative"] = ""
+                if len(comLike_tmp)>0:
+                    comLike=comLike_tmp[0]
+                    posi = int(comLike.xpath('./li[@class="positive"]/a/span')[0].text)
+                    nega = int(comLike.xpath('./li[@class="negative"]/a/span')[0].text)
+                    hash["positive"] = posi
+                    hash["negative"] = nega
                 self.write(filename, hash)
                 iswrite = True
 
-        except:
+        except Exception as e:
+
             print "Unexpected error:", sys.exc_info()[0]
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print str(e.args)
+            print e.message
             iswrite = False
         return iswrite
 
     def getfilename(self, url, num):
         path = self.outdir + re.sub("\?.*$", "", re.sub("^.*\/message", "", url))
-        filename = path + "/" + num.to_s + ".json"
+        filename = path + "/" + str(num) + ".json"
         return filename
 
     def write(self, filename, hash):
         path = os.path.dirname(filename)
-        # path=@outdir+url.gsub(/^.*\/message/,"")
-        os.makedirs(path)
-        # open(path+"/"+num.to_s+".json","w") do |f|
+        if not os.path.exists(path):
+            os.makedirs(path)
         with open(filename, "w") as f:
-            f.puts(json.dumps(hash))
+            f.write(json.dumps(hash, ensure_ascii=False))
 
     def trim(self, s):
         tmp = s.rstrip
@@ -109,11 +158,13 @@ class YahooBoard:
         return tmp
 
     def prev_url(self, url):
-        doc = urllib.urlopen(url)
-        lst = doc.xpath('//div[@id="toppg"]/div/ul').xpath("./li[2]/a")
+        html = urllib.urlopen(url).read()
+        doc = lxml.html.fromstring(html)
+        li = doc.xpath('//div[@id="toppg"]/div/ul/li')
+        lst=li[2].xpath('./a/@href')
 
         for v in lst:
-            return v["href"]
+            return v
 
         return None
 
@@ -121,18 +172,20 @@ class YahooBoard:
     def conv_ymd(self, ymd):
         # "ymd=2015年4月28日 06:22"
         # p "ymd="+ymd
-        if ymd.include("年"):
+        if ymd.find("年")>=0:
             dt = re.sub("年", "-", ymd)
             dt = re.sub("月", "-", dt)
             dt = re.sub("日", "", dt) + ":00"
             now = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-            return now.strftime("%Y-%m-%d %H:%M:%S")
+            return now
+            #return now.strftime("%Y-%m-%d %H:%M:%S")
         else:
             dt = re.sub("月", "-", ymd)
             dt = re.sub("日", "", dt) + ":00"
             dt = datetime.datetime.now().strftime("%Y") + "-" + dt
             now = datetime.datetime.strptime(dt, '%Y-%m-%d %H:%M:%S')
-            return now.strftime("%Y-%m-%d %H:%M:%S")
+            #return now.strftime("%Y-%m-%d %H:%M:%S")
+            return now
 
 
 if __name__ == '__main__':
