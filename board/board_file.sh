53f6315840a77cd4d90bf34048a6020aa6b2bed3
靱 勝彦 <utsubo@qri.jp>
2016-11-02 14:24:06 +0900

    add

--- board/board_file.sh
+++ board/board_file.sh
@@ -7,13 +7,13 @@ print "$1".split("/")[4][3..6]
 Eof
 }
 
-urls=`cat board_file.txt`
+urls=`cat board_file.dat`
 
 for url in $urls;do
   code=`get_code $url`
   for i in `seq 1 100`;do
-    echo "ruby board.rb /home/admin/data/board $code $url/$i"
-    ruby board.rb /home/admin/data/board $code $url/$i
+    echo "python board.py /home/admin/data/board $code $url/$i"
+    python board.py /Volumes/admin/data $code $url/$i
     if [ $? = 1 ];then
       break
     fi
